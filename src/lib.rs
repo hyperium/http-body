4a24b020f74fa60049fea506f56bf94baf2706e3
Sean McArthur <sean@seanmonstar.com>
2019-10-01 08:44:50 -0700

    v0.2.0-alpha.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/http-body/0.2.0-alpha.1")]
+#![doc(html_root_url = "https://docs.rs/http-body/0.2.0-alpha.2")]
 #![deny(missing_debug_implementations, missing_docs, unreachable_pub)]
 #![cfg_attr(test, deny(warnings))]
 
