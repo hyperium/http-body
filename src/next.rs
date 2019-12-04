69863ea35887572a877cbb29ccad40cd8e19be82
Sean McArthur <sean@seanmonstar.com>
2019-12-04 14:51:10 -0800

    Rename the 'next' combinator to 'data'

--- src/next.rs
+++ src/next.rs
@@ -1,16 +1,15 @@
-//! Next futures for `Body`
-
 use crate::Body;
 
 use core::future::Future;
 use core::pin::Pin;
 use core::task;
 
+#[must_use = "futures don't do anything unless polled"]
 #[derive(Debug)]
 /// Future that resolves to the next data chunk from `Body`
-pub struct Next<'a, T: ?Sized>(pub(crate) &'a mut T);
+pub struct Data<'a, T: ?Sized>(pub(crate) &'a mut T);
 
-impl<'a, T: Body + Unpin + ?Sized> Future for Next<'a, T> {
+impl<'a, T: Body + Unpin + ?Sized> Future for Data<'a, T> {
     type Output = Option<Result<T::Data, T::Error>>;
 
     fn poll(mut self: Pin<&mut Self>, ctx: &mut task::Context<'_>) -> task::Poll<Self::Output> {
@@ -18,6 +17,7 @@ impl<'a, T: Body + Unpin + ?Sized> Future for Next<'a, T> {
     }
 }
 
+#[must_use = "futures don't do anything unless polled"]
 #[derive(Debug)]
 /// Future that resolves to the optional trailers from `Body`
 pub struct Trailers<'a, T: ?Sized>(pub(crate) &'a mut T);
