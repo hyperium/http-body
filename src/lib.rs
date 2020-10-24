f678fe292b15506109aa52158c81a90e4f60aa19
Paolo Barbolini <paolo@paolo565.org>
2020-10-24 18:11:58 +0200

    Bump bytes crate to 0.6

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/http-body/0.3.1")]
+#![doc(html_root_url = "https://docs.rs/http-body/0.4.0")]
 #![deny(missing_debug_implementations, missing_docs, unreachable_pub)]
 #![cfg_attr(test, deny(warnings))]
 
