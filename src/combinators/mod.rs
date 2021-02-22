d8c8124965d7b721b14698bc1fab79b0094641ac
David Pedersen <david.pdrsn@gmail.com>
2021-02-22 19:40:22 +0100

    Format

--- src/combinators/mod.rs
+++ src/combinators/mod.rs
@@ -5,6 +5,4 @@ mod into_stream;
 mod map_data;
 mod map_err;
 
-pub use self::{
-    box_body::BoxBody, into_stream::IntoStream, map_data::MapData, map_err::MapErr,
-};
+pub use self::{box_body::BoxBody, into_stream::IntoStream, map_data::MapData, map_err::MapErr};
