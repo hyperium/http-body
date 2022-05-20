730e9bd64caaa07a0724d4624d55f809c6b71873
Lucio Franco <luciofranco14@gmail.com>
2022-05-20 14:11:48 -0400

    Add String impl for body (#58)

--- src/lib.rs
+++ src/lib.rs
@@ -26,8 +26,9 @@ pub use self::next::{Data, Trailers};
 pub use self::size_hint::SizeHint;
 
 use self::combinators::{BoxBody, MapData, MapErr, UnsyncBoxBody};
-use bytes::Buf;
+use bytes::{Buf, Bytes};
 use http::HeaderMap;
+use std::convert::Infallible;
 use std::ops;
 use std::pin::Pin;
 use std::task::{Context, Poll};
@@ -280,6 +281,38 @@ impl<B: Body> Body for http::Response<B> {
 
     fn size_hint(&self) -> SizeHint {
         self.body().size_hint()
+    }
+}
+
+impl Body for String {
+    type Data = Bytes;
+    type Error = Infallible;
+
+    fn poll_data(
+        mut self: Pin<&mut Self>,
+        _cx: &mut Context<'_>,
+    ) -> Poll<Option<Result<Self::Data, Self::Error>>> {
+        if !self.is_empty() {
+            let s = std::mem::take(&mut *self);
+            Poll::Ready(Some(Ok(s.into_bytes().into())))
+        } else {
+            Poll::Ready(None)
+        }
+    }
+
+    fn poll_trailers(
+        self: Pin<&mut Self>,
+        _cx: &mut Context<'_>,
+    ) -> Poll<Result<Option<HeaderMap>, Self::Error>> {
+        Poll::Ready(Ok(None))
+    }
+
+    fn is_end_stream(&self) -> bool {
+        self.is_empty()
+    }
+
+    fn size_hint(&self) -> SizeHint {
+        SizeHint::with_exact(self.len() as u64)
     }
 }
 
