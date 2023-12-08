c800ee97abbf2fe571d7d93f34dd950a850346b7
Sean McArthur <sean@seanmonstar.com>
2023-12-08 09:02:07 -0500

    v0.4.6

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/http-body/0.4.5")]
+#![doc(html_root_url = "https://docs.rs/http-body/0.4.6")]
 #![deny(
     missing_debug_implementations,
     missing_docs,
